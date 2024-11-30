e6d92900d7a6e3c9f6aeb1c1ea9279b5008ee735
dasha-s123 <sevostianova.darya@gmail.com>
2024-11-30 22:04:08 +0300

    Update 8.7.py
    Добавила необязательный аргумент о количестве песен в альбоме. Изменила вывод словарей, сохранив их в переменных

--- chapter8/lesson3/8.7.py
+++ chapter8/lesson3/8.7.py
@@ -1,8 +1,17 @@
-def make_album(name_of_singer, name_of_album):
+def make_album(name_of_singer, name_of_album, count_of_songs=None):
     """возвращает словарь с именем музыканта и названием альбома"""
     music = {"singer": name_of_singer.title(), "name": name_of_album}
+    if count_of_songs:
+        music["count_of_songs"] = count_of_songs
     return music
 
 
-print(make_album('noize', 'последний альбом')) #создаю и вывожу словари для разных альбомов
-print(make_album('валентин стрыкало', 'смирись и расслабься'))
+#создаю словари под разные альбомы
+album_1 = make_album('noize', 'последний альбом')
+album_2 = make_album('валентин стрыкало', 'смирись и расслабься', 14)
+album_3 = make_album(name_of_album='EP#3', name_of_singer='Электрофорез', count_of_songs=4)
+
+#печатаю информацию об альбомах
+print(album_1)
+print(album_2)
+print(album_3)
