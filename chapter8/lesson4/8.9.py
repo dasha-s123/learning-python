aa5f7174494db2daa60c7fc7aeeab6000035a889
dasha-s123 <sevostianova.darya@gmail.com>
2024-12-02 00:22:05 +0300

    Добавила 8.9 и урок4

---
+++ chapter8/lesson4/8.9.py
@@ -0,0 +1,8 @@
+def show_message(list):
+    """выводит сообщения из списка"""
+    for message in list:
+        print(message)
+
+
+messages = ['I love python', 'My name is dasha', 'I read a book rn']
+show_message(list=messages)
