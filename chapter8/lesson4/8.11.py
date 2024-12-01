3b4a42656740071918cf5a2574b876432a2e1c86
dasha-s123 <sevostianova.darya@gmail.com>
2024-12-02 00:34:51 +0300

    Create 8.11.py

---
+++ chapter8/lesson4/8.11.py
@@ -0,0 +1,18 @@
+def send_messages(list_full, list_empty):
+    """перемещает сообщения, отправляет их"""
+
+    # Цикл последовательно отправляет сообщения
+    # А затем перемещает их в список отправленных сообщений
+    while list_full:
+        message = list_full.pop()
+        print(message)
+        list_empty.append(message)
+
+
+messages_to_send = ['I love python', 'My name is dasha', 'I read a book rn']
+sent_messages = []
+
+# Вызываю функцию работая с копией списка
+send_messages(list_full=messages_to_send[:], list_empty=sent_messages)
+print(f'\n{messages_to_send}')
+print(sent_messages)
